d34d3e79888ea74bd022148800bbf068319e45a5
Md Azharul Hoque <mdazharulhoque7@gmail.com>
2026-05-25 03:22:10 +0600

    Theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:hqexpensetracker/widgets/expenses/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 3, 67, 24));
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 0, 31, 11));
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer
+        ),
+        cardTheme: CardThemeData().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8)
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.onPrimaryContainer,
+            foregroundColor: kDarkColorScheme.primaryContainer
+          )
+        )
+      ),
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer
+        ),
+        cardTheme: CardThemeData().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8)
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.onPrimaryContainer,
+            foregroundColor: kDarkColorScheme.primaryContainer
+          )
+        )
+      ),
+      // themeMode: ThemeMode.system,
       home: ExpensesApp(),
     ),
   );
--- lib/widgets/expenses/expenses_list/expenses_list.dart
+++ lib/widgets/expenses/expenses_list/expenses_list.dart
@@ -19,6 +19,11 @@ class ExpensesList extends StatelessWidget {
       itemBuilder: (ctx, index) => Dismissible(
         key: ValueKey(expenses[index]),
         onDismissed: (direction) => onRemoveExpense(expenses[index]),
+        background: Container(
+          color: Theme.of(context).colorScheme.primary.withAlpha(50),
+          margin: EdgeInsets.symmetric(
+            horizontal: Theme.of(context).cardTheme.margin!.horizontal)
+        ),
         child: ExpenseItem(expenses[index]),
       ),
     );
